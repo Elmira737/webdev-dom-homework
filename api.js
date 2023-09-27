94855f455ba9a10651ca9795e3ac1e1441a39ba6
Elmira737 <139793523+Elmira737@users.noreply.github.com>
2023-09-28 02:37:10 +0500

    Change the file api.js

---
+++ api.js
@@ -0,0 +1,21 @@
+export function getComments() {
+    return fetch('https://wedev-api.sky.pro/api/v1/elmira-kolchanova/comments', {
+        method: 'GET',
+    }).then((response) => {
+        if (response.status === 500) {
+            throw new Error('Сервер сломался, попробуй позже');
+        }
+        return response.json();
+    });
+}
+
+export function postComment({ name, text }) {
+    return fetch('https://wedev-api.sky.pro/api/v1/elmira-kolchanova/comments', {
+        method: 'POST',
+        body: JSON.stringify({
+            text: text,
+            name: name,
+            forceError: false,
+        }),
+    })
+}
--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 "use strict";
-  const nameInput = document.querySelector('.add-form-name');
-  const commentInput = document.querySelector('.add-form-text');
-  const addButton = document.querySelector('.add-form-button');
-  const commentsList = document.querySelector('.comments');
-  const deleteButtonElement = document.querySelector('.delete-form-button');
-  const container = document.querySelector('.container');
-  const addForm = document.querySelector('.add-form');
-  const pageLoader = document.querySelector('.page-loader');
-  const commentLoader = document.querySelector('.comment-loader');
+import { getComments, postComment } from "./api.js";
 
-  addButton.addEventListener("click", () => {
+const nameInput = document.querySelector('.add-form-name');
+const commentInput = document.querySelector('.add-form-text');
+const addButton = document.querySelector('.add-form-button');
+const commentsList = document.querySelector('.comments');
+const deleteButtonElement = document.querySelector('.delete-form-button');
+const container = document.querySelector('.container');
+const addForm = document.querySelector('.add-form');
+const pageLoader = document.querySelector('.page-loader');
+const commentLoader = document.querySelector('.comment-loader');
+
+addButton.addEventListener("click", () => {
     nameInput.classList.remove("error");
     commentInput.classList.remove("error");
     if (nameInput.value === '' || commentInput.value === '') {
-      nameInput.classList.add("error");
-      commentInput.classList.add("error");
-      return;
+        nameInput.classList.add("error");
+        commentInput.classList.add("error");
+        return;
     }
 
     addComment(nameInput.value, commentInput.value);
@@ -26,136 +28,123 @@
 
     nameInput.value = '';
     commentInput.value = '';
-  });
+});
 
-  const formatDate = (date) => {
+const formatDate = (date) => {
     const options = { year: '2-digit', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric' };
     return date.toLocaleString('ru', options).replace(',', '');
-  };
+};
 
-  let comments = [];
+let comments = [];
 
-  const getApiData = () => {
+const getApiData = () => {
     container.classList.add('delete');
     pageLoader.textContent = `Пожалуйста, подождите. Загружаю комментарии...`;
-    return fetch('https://wedev-api.sky.pro/api/v1/elmira-kolchanova/comments', {
-      method: 'GET',
-    }).then((response) => {
-      if (response.status === 500) {
-        throw new Error('Сервер сломался, попробуй позже');
-      } 
-      return response.json()
-        .then((responseData) => {
-          comments = responseData.comments.map((comment) => {
+    getComments().then((responseData) => {
+        comments = responseData.comments.map((comment) => {
             return {
-              name: comment.author.name,
-              date: new Date(comment.date),
-              text: comment.text,
-              likes: comment.likes,
-              isLiked: false,
+                name: comment.author.name,
+                date: new Date(comment.date),
+                text: comment.text,
+                likes: comment.likes,
+                isLiked: false,
             };
-          });
-          renderComments(comments);
         });
+        renderComments(comments);
+        pageLoader.textContent = "";
+        pageLoader.classList.add('delete');
+        container.classList.remove('delete');
     })
-      .then((response) => {
-        pageLoader.textContent = "";
-        pageLoader.classList.add('delete');
-        container.classList.remove('delete');
-      })
-      .catch((error) => {
-        alert('Кажется, у вас сломался интернет, попробуйте позже');
-        console.warn(error);
-        pageLoader.textContent = "";
-        pageLoader.classList.add('delete');
-        container.classList.remove('delete');
-      });
-  };
+        .catch((error) => {
+            alert('Кажется, у вас сломался интернет, попробуйте позже');
+            console.warn(error);
+            pageLoader.textContent = "";
+            pageLoader.classList.add('delete');
+            container.classList.remove('delete');
+        });
+}
 
-  const escapeHTML = (text) => {
+const escapeHTML = (text) => {
     const entitiesMap = new Map([
-      ['&', '&amp;'],
-      ['<', '&lt;'],
-      ['>', '&gt;'],
-      ['"', '&quot;'],
-      ["'", '&#39;']
+        ['&', '&amp;'],
+        ['<', '&lt;'],
+        ['>', '&gt;'],
+        ['"', '&quot;'],
+        ["'", '&#39;']
     ]);
 
     return Array.from(entitiesMap.keys()).reduce((acc, key) => {
-      return acc.replaceAll(key, entitiesMap.get(key));
+        return acc.replaceAll(key, entitiesMap.get(key));
     }, text);
-  };
+};
 
-  const addComment = async (name, text, likes = 0, date = new Date(), isLiked = false) => {
+const addComment = async (name, text, likes = 0, date = new Date(), isLiked = false) => {
     const safeNameInputValue = escapeHTML(name);
     const safeCommentInputValue = escapeHTML(text);
 
-    fetch('https://wedev-api.sky.pro/api/v1/elmira-kolchanova/comments', {
-      method: 'POST',
-      body: JSON.stringify({
-        text: safeCommentInputValue,
+    postComment({
         name: safeNameInputValue,
-        forceError: true,
-      }),
-    }).then((response) => {
-      if (response.status === 500) {
-        alert('Сервер сломался, попробуй позже');
-        commentLoader.classList.add('delete');
-        addForm.classList.remove('delete');
-        nameInput.value = safeNameInputValue;
-        commentInput.value = safeCommentInputValue;
-      } else if (response.status === 400) {
-        alert('Имя и комментарий должны быть не короче 3 символов');
-        commentLoader.classList.add('delete');
-        addForm.classList.remove('delete');
-        nameInput.value = safeNameInputValue;
-        commentInput.value = safeCommentInputValue;
-      } else {
-        getApiData();
-
-        commentLoader.classList.add('delete');
-        addForm.classList.remove('delete');
-      }
+        text: safeCommentInputValue,
     })
-      .catch((error) => {
-        alert('Кажется, у вас сломался интернет, попробуйте позже');
-        console.warn(error);
-        commentLoader.classList.add('delete');
-        addForm.classList.remove('delete');
-        nameInput.value = safeNameInputValue;
-        commentInput.value = safeCommentInputValue;
-      });
-  }
-  getApiData();
+        .then((response) => {
+            if (response.status === 500) {
+                alert('Сервер сломался, попробуй позже');
+                commentLoader.classList.add('delete');
+                addForm.classList.remove('delete');
+                nameInput.value = safeNameInputValue;
+                commentInput.value = safeCommentInputValue;
+            } else if (response.status === 400) {
+                alert('Имя и комментарий должны быть не короче 3 символов');
+                commentLoader.classList.add('delete');
+                addForm.classList.remove('delete');
+                nameInput.value = safeNameInputValue;
+                commentInput.value = safeCommentInputValue;
+            } else {
+                getApiData();
 
-  const toggleLikeComment = (commentName) => {
+                commentLoader.classList.add('delete');
+                addForm.classList.remove('delete');
+            }
+        })
+        .catch((error) => {
+            alert('Кажется, у вас сломался интернет, попробуйте позже');
+            console.warn(error);
+            commentLoader.classList.add('delete');
+            addForm.classList.remove('delete');
+            nameInput.value = safeNameInputValue;
+            commentInput.value = safeCommentInputValue;
+        });
+}
+getApiData();
+
+const toggleLikeComment = (commentName) => {
     comments.forEach((comment, i) => {
-      if (comment.name === commentName) {
-        comments[i] = { ...comment, isLiked: !comment.isLiked, likes: comment.isLiked ? comment.likes - 1 : comment.likes + 1 };
-      }
+        if (comment.name === commentName) {
+            comments[i] = { ...comment, isLiked: !comment.isLiked, likes: comment.isLiked ? comment.likes - 1 : comment.likes + 1 };
+        }
     });
 
     renderComments(comments);
-  };
+};
 
-  const autofillForm = (name, text) => {
+const autofillForm = (name, text) => {
     commentInput.value = `> ${text}\n${name}\n\n`;
     commentInput.focus();
-  };
+};
 
-  const renderComments = (comments) => {
+const renderComments = (comments) => {
     const allComments = commentsList.querySelectorAll('.comment');
 
     if (allComments.length >= 1) {
-      commentsList.innerHTML = '';
+        commentsList.innerHTML = '';
     }
 
     if (Array.isArray(comments)) {
-      for (let comment of comments) {
-        const newComment = document.createElement('li');
-        newComment.classList.add('comment');
+        for (let comment of comments) {
+            const newComment = document.createElement('li');
+            newComment.classList.add('comment');
 
-        newComment.innerHTML = `
+            newComment.innerHTML = `
               <div class="comment-header">
                 <div>${escapeHTML(comment.name)}</div>
                 <div>${formatDate(comment.date)}</div>
@@ -173,28 +162,28 @@
               </div>
         `;
 
-        commentsList.appendChild(newComment);
+            commentsList.appendChild(newComment);
 
-        newComment.querySelector('.like-button').addEventListener('click', (e) => {
-          e.stopPropagation();
-          toggleLikeComment(comment.name);
-        });
+            newComment.querySelector('.like-button').addEventListener('click', (e) => {
+                e.stopPropagation();
+                toggleLikeComment(comment.name);
+            });
 
-        newComment.addEventListener('click', () => {
-          autofillForm(comment.name, comment.text);
-        });
-      }
+            newComment.addEventListener('click', () => {
+                autofillForm(comment.name, comment.text);
+            });
+        }
     }
-  };
+};
 
-  renderComments(comments);
+renderComments(comments);
 
-  deleteButtonElement.addEventListener('click', () => {
+deleteButtonElement.addEventListener('click', () => {
     const askForDeleteComment = confirm('Вы уверены, что хотите удалить последний комментарий?');
     if (askForDeleteComment) {
-      comments.pop();
-      renderComments(comments);
+        comments.pop();
+        renderComments(comments);
     }
-  });
+});
 
-  console.log("It works!");
+console.log("It works!");
